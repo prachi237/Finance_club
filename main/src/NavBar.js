95b5ec121a56049e929b3917a4088770c46015e9
Prachi Nandi <prachinandi237@gmail.com>
2021-08-02 00:12:05 +0530

    monirchnage

--- main/src/NavBar.js
+++ main/src/NavBar.js
@@ -13,7 +13,9 @@ function NavBar(){
                 <li><a href="/">Team</a></li>
                 <li><a href="/">Contact</a></li>
             </ul>
+            
         </nav>
+        
     </div>
 }
 export default NavBar;
