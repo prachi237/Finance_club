e54beab9785449d34c0b62cba48293fe2baa722a
Prachi Nandi <prachinandi237@gmail.com>
2021-09-28 02:40:55 +0530

    folders

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/contact" exact component={Contact} />
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import main from "../assets/hOME.jpeg";
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div className="home" style={{ backgroundImage: `url(${main})` }}>
+      <div className="headerContainer">
+        <h1> Finance Club </h1>
+        <p> NIT ROURKELA</p>
+        <Link to="/about">
+          <button> KNOW MORE </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
