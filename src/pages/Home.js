391573c4bdba34c2ed94cb07490150fa2633f2c3
Prachi Nandi <prachinandi237@gmail.com>
2021-09-28 03:46:14 +0530

    edit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,12 +6,17 @@ import "../styles/Home.css";
 function Home() {
   return (
     <div className="home" style={{ backgroundImage: `url(${main})` }}>
+      <div className="leftside">
       <div className="headerContainer">
         <h1> Finance Club </h1>
         <p> NIT ROURKELA</p>
         <Link to="/about">
           <button> KNOW MORE </button>
         </Link>
+      </div>
+      </div>
+      <div className="rightside">
+       
       </div>
     </div>
   );
