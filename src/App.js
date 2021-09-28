ab9c0f97e32921b4e59f8b68206afdd91bcd8954
Prachi Nandi <prachinandi237@gmail.com>
2021-09-28 07:46:44 +0530

    patch2

--- src/App.js
+++ src/App.js
@@ -9,11 +9,10 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      
-      <Router>
+        <Router>
         <Navbar />
-        <Home/>
         <Switch>
+          <Route path="/" exact component={Home} />
           <Route path="/about" exact component={About} />
           <Route path="/contact" exact component={Contact} />
         </Switch>
